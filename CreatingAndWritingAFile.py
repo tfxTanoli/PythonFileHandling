453068ad16f258decab218a09521a37d7eb4f17f
tfxTanoli <usman5194999@gmail.com>
2024-01-26 08:24:13 +0500

    Added Comments

--- CreatingAndWritingAFile.py
+++ CreatingAndWritingAFile.py
@@ -1,5 +1,9 @@
 # Create and Write In A File
-file = open("demo.txt", "w") #use "a" for append
+#use "a" for append
+#use "x" for creating new empty file
+#use "w" for overwriting a file
+
+file = open("demo.txt", "w")
 try:
     file.write("FA21-BCS-130 Muhammad Usman")
     print("File Created Successfully.")
